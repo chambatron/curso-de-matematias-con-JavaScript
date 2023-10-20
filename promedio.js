72711831bd3a59a3d74936495ee9748cf6bc0557
Salvador Samayoa <samayoash@gmail.com>
2023-10-19 23:00:08 -0600

    usando reduce

--- promedio.js
+++ promedio.js
@@ -1,10 +1,16 @@
 function calcularPromedio(lista){
     //sumar todos los elemmentos del array y dividir entre la cantidad de elemantos
-let sumaLista = 0;
-
+/*let sumaLista = 0;
     for (let i = 0; i < lista.length; i++){
         sumaLista = sumaLista + lista[i];
+    }*/
+
+    function sumarElementos(varlorAcumulado, nuevoValor){
+        return varlorAcumulado + nuevoValor;
     }
+
+    const sumaLista = lista.reduce(sumarElementos);
+
     const promedio = sumaLista/ lista.length;
     console.log(promedio);
     return promedio;
